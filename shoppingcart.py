304a20f8025451f94e9a2d4ac83127ad3d0e1f7a
ttodawg01 <tonycampos1803@gmail.com>
2022-10-04 22:15:42 -0500

    revising

--- shoppingcart.py
+++ shoppingcart.py
@@ -6,9 +6,8 @@
 # 6) Upon quiting the program, prints out a receipt of the items with total and quantity.
 
 def shopping_cart():
-    full_cart = []
+    full_cart = {}
     while True:
-
 
         carts = input("What would you like to do? add/remove/show/clear/quit ")
         if carts == "add":
@@ -16,10 +15,23 @@ def shopping_cart():
             price = input(f"What is the price of {item} ?")
             total = input(f"How many {item}'s would you like to add? ")
             print(f"{total} {item}'s have been added to your cart")
+            full_cart[item] = {}
+            full_cart[item]["quantity"]= int(total)
+            full_cart[item]["costs"] = price
+            print(full_cart)
 
         elif carts == "remove":
-            get_out = input(f"How many of the {total} {item}'s would you like to remove? ")
+            remove_item = input("What item would you like to remove? ")
+            get_out = input(f"How many of the {remove_item}'s would you like to remove? ")
+            # current_quantity = int(full_cart[remove_item]["quantity"])
+            # print(current_quantity)
             print(f"{get_out} {item}'s have been removed from your cart")
+            if int(get_out) > full_cart[remove_item]["quantity"]:
+                del full_cart [remove_item]
+                print(full_cart)
+            else:
+                full_cart[item]["quantity"] -= int(get_out)
+                print(full_cart)
             
         elif carts == "clear":
             you_sure = input("are you sure? Once you clear you cannot undo, Y/N ")
@@ -31,10 +43,23 @@ def shopping_cart():
         elif carts == "quit":
             print("Thank you for shopping with us")
             print("your reciept: ")
+            total_cost = 0
+            total_quantity = 0 
+            for x in full_cart:
+             
+                full_cart[x]["quantity"]
+                total_quantity += full_cart[x]["quantity"]
+            print(f"this is the amount you ordered {total_quantity}")
+                
             break
+
+        elif carts == "show":
+            print(full_cart)
 
         while carts not in {"add", "remove", "show", "clear", "quit"}:
             carts = input("That's not a valid response. What would you like to do? add/remove/show/clear/quit ")
+
+
         # elif carts == "clear":
         #     put_back = input("What item would you like to remove in the cart? ")
         #     print(f"{put_back} is not in your cart")
